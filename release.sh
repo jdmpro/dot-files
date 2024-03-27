16e1918079b0b2a70e32f4217d8351ea4b78f1e7
jdmpro <58115966+jdmpro@users.noreply.github.com>
2024-03-26 22:33:56 -0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -13,33 +13,33 @@ RESET=$(tput sgr0)
 printf "${NOTE} Downloading / Checking for existing dotfiles.tar.gz...\n"
 
 # Check if Hyprland-Dots.tar.gz exists
-if [ -f dotfiles.tar.gz ]; then
+if [ -f dot-files.tar.gz ]; then
   printf "${NOTE} dotfiles.tar.gz found.\n"
 
   # Get the version from the existing tarball filename
-  existing_version=$(echo dotfiles.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
+  existing_version=$(echo dot-files.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
 
   # Fetch the tag_name for the latest release using the GitHub API
-  latest_version=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
+  latest_version=$(curl -s https://api.github.com/repos/jdmpro/dot-files/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
 
   # Check if versions match
   if [ "$existing_version" = "$latest_version" ]; then
-    echo -e "${OK} dotfiles.tar.gz is up-to-date with the latest release ($latest_version)."
+    echo -e "${OK} dot-files.tar.gz is up-to-date with the latest release ($latest_version)."
     
     # Sleep for 10 seconds before exiting
     printf "${NOTE} No update found. Sleeping for 10 seconds...\n"
     sleep 10
     exit 0
   else
-    echo -e "${WARN} Hyprland-Dots.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
+    echo -e "${WARN} dot-files.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
     read -p "Do you want to upgrade to the latest version? (y/n): " upgrade_choice
     if [ "$upgrade_choice" = "y" ]; then
 		echo -e "${NOTE} Proceeding to download the latest release."
 		
-		# Delete existing directories starting with shredder-dotfiles
-      find . -type d -name 'jdmpro-dotfiles*' -exec rm -rf {} +
-      rm -f dotfiles.tar.gz
-      printf "${WARN} Removed existing dotfiles.tar.gz.\n"
+		# Delete existing directories starting with shredder-dot-files
+      find . -type d -name 'jdmpro-dot-files*' -exec rm -rf {} +
+      rm -f dot-files.tar.gz
+      printf "${WARN} Removed existing dot-files.tar.gz.\n"
     else
       echo -e "${NOTE} User chose not to upgrade. Exiting..."
       exit 0
@@ -50,7 +50,7 @@ fi
 printf "${NOTE} Downloading the latest Hyprland source code release...\n"
 
 # Fetch the tag name for the latest release using the GitHub API
-latest_tag=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+latest_tag=$(curl -s https://api.github.com/repos/jdmpro/dot-files/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 
 # Check if the tag is obtained successfully
 if [ -z "$latest_tag" ]; then
@@ -59,7 +59,7 @@ if [ -z "$latest_tag" ]; then
 fi
 
 # Fetch the tarball URL for the latest release using the GitHub API
-latest_tarball_url=$(curl -s https://api.github.com/repos/jdmpro/dotfiles/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+latest_tarball_url=$(curl -s https://api.github.com/repos/jdmpro/dot-files/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
 
 # Check if the URL is obtained successfully
 if [ -z "$latest_tarball_url" ]; then
@@ -68,7 +68,7 @@ if [ -z "$latest_tarball_url" ]; then
 fi
 
 # Get the filename from the URL and include the tag name in the file name
-file_name="dotfiles-${latest_tag}.tar.gz"
+file_name="dot-files-${latest_tag}.tar.gz"
 
 # Download the latest release source code tarball to the current directory
 if curl -L "$latest_tarball_url" -o "$file_name"; then
@@ -76,15 +76,15 @@ if curl -L "$latest_tarball_url" -o "$file_name"; then
   tar -xzf "$file_name" || exit 1
 
   # delete existing Hyprland-Dots
-  rm -rf jdmpro-dotfiles
+  rm -rf jdmpro-dot-files
 
   # Identify the extracted directory
   extracted_directory=$(tar -tf "$file_name" | grep -o '^[^/]\+' | uniq)
 
   # Rename the extracted directory to JaKooLit-Hyprland-Dots
-  mv "$extracted_directory" jdmpro-dotfiles || exit 1
+  mv "$extracted_directory" jdmpro-dot-files || exit 1
 
-  cd "jdmpro-dotfiles" || exit 1
+  cd "jdmpro-dot-files" || exit 1
 
   # Set execute permission for copy.sh and execute it
   chmod +x copy.sh
